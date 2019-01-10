ac81fe9b192707537fd0229972a905298f3abeba
Unknown <gilles.labrousse@etu.univ-tlse2.fr>
2019-01-10 17:25:21 +0100

    Ajout mandat et promesse de vente

--- src/BienImmobilier.java
+++ src/BienImmobilier.java
@@ -7,6 +7,7 @@ public abstract class BienImmobilier {
     private String localistation, orientation;
     private Date dateDeVenteSouhaitee, dateDispo;
     Mandat mandat;
+    PromesseVente promesseVente;
     Personne vendeur;
 
     public BienImmobilier(int idBien, int prix, String localistation, Date dateDeVenteSouhaitee, Date dateDispo, String orientation, Personne vendeur) {
@@ -40,8 +41,8 @@ public abstract class BienImmobilier {
         mandat = new Mandat(vendeur, prix, dateDeVenteSouhaitee, dateFinMendat);
     }
 
-    public void signerPromesseDeVente(Personne acheteur){
+    public void signerPromesseDeVente(Personne acheteur, double prix, String adresseNotaire, Date dateVente, Double fraisDeVente){
         acheteur.AjouterBienAAcheter(this);
-
+        promesseVente = new PromesseVente(acheteur, prix, adresseNotaire, dateVente, fraisDeVente);
     }
 }
