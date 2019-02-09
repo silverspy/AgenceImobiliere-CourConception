d829c5981dede9299c5aafbf377430848003ff1d
Gilles Labrousse <gilles.labrousse@etu.univ-tlse2.fr>
2019-02-09 15:58:21 +0100

    BienImmobilier test

---
+++ src/BienImmobilierTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BienImmobilierTest {
+
+    @Test
+    public void getPrix() {
+    }
+
+    @Test
+    public void getLocalistation() {
+    }
+
+    @Test
+    public void getIdBien() {
+    }
+
+    @Test
+    public void inscrire() {
+    }
+
+    @Test
+    public void signermandat() {
+    }
+
+    @Test
+    public void signerPromesseDeVente() {
+    }
+
+    @Test
+    public void getVendeur() {
+    }
+}
