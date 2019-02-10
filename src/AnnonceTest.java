f5f84401f1c0b9315b67cdb0c572f122f7aec6a7
Gilles Labrousse <gilles.labrousse@etu.univ-tlse2.fr>
2019-02-10 16:37:42 +0100

    Create AnnonceTest

---
+++ src/AnnonceTest.java
@@ -0,0 +1,17 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AnnonceTest {
+
+    @Before
+    public void setUp() throws Exception {
+        Annonce a = new Annonce();
+    }
+
+    @Test
+    public void createAnnonce(){
+        int a = 1;
+    }
+}
